2ae1e457af62cdccd45148a994e7912b19a22ebb
Alper ARI <alper.hamza.ari@gmail.com>
2023-01-03 22:38:14 +0300

    Init project

---
+++ main.py
@@ -0,0 +1,51 @@
+import sys
+import os
+import random
+import threading
+from multiprocessing import Process, Value, Array
+import zmq
+import time
+
+
+def PaxosNode(node_id, value, numProc, prob, numRounds):
+    pass
+
+
+def main(args):
+    numProc = int(args[1])
+    prob = float(args[2])
+    numRounds = int(args[3])
+
+    # Create processes
+    # Each process represents a paxos node
+    processes = []
+
+    for node_id in numProc:
+        value = random.randint(0, 1)
+        process = Process(
+            target=PaxosNode,
+            args=(
+                node_id,
+                value,
+                numProc,
+                prob,
+                numRounds,
+            ),
+        )
+        processes.append(process)
+
+    for process in processes:
+        process.start()
+
+    # Wait all paxos nodes to finish rounds
+    for process in processes:
+        process.join()
+
+    pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Invalid command line arguments!")
+    else:
+        main(args=sys.argv)
